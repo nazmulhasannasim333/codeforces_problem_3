93fcc2ca8403c4907ed8d14ce0fb098245d6d77d
Nasim <mdnasimhosen333@gmail.com>
2025-04-17 22:53:09 +0600

    solved problem searching

---
+++ B_Searching.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    int A[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+
+    int X;
+    scanf("%d", &X);
+
+    for (int i = 0; i < N; i++)
+    {
+        if (A[i] == X)
+        {
+            printf("%d\n", i);
+            return 0;
+        }
+    }
+
+    printf("-1\n");
+    return 0;
+}
