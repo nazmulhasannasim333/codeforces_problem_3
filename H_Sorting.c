a9a1ef3b27a8c99f32ac5f16989fe7c57dace179
Nasim <mdnasimhosen333@gmail.com>
2025-04-18 09:43:50 +0600

    solved problem 8

---
+++ H_Sorting.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    int A[N];
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            if (A[i] > A[j])
+            {
+                int temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+    }
+    for (int i = 0; i < N; i++)
+    {
+        printf("%d ", A[i]);
+    }
+
+    return 0;
+}
